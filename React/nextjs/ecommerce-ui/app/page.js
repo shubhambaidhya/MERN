2f47852230d95e9e138016877bd5ae6334b0b562
shubhambaidhya <shubhambaidhya7@gmail.com>
2024-12-23 15:53:36 +0545

    Backend and frontend route

--- React/nextjs/ecommerce-ui/app/page.js
+++ React/nextjs/ecommerce-ui/app/page.js
@@ -1,12 +1,13 @@
 'use client';
 import BuyerList from '@/components/BuyerList';
 import SellerList from '@/components/SellerList';
+import { red } from '@mui/material/colors';
 
 const Home = () => {
   const userRole = localStorage.getItem('userRole');
   return (
     <div>
-      <p className="text-5xl bold ">
+      <p className="text-5xl bold">
         Welcome {localStorage.getItem('firstName')}
       </p>
 
--- React/nextjs/ecommerce-ui/components/BuyerList.jsx
+++ React/nextjs/ecommerce-ui/components/BuyerList.jsx
@@ -1,7 +1,12 @@
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 const BuyerList = () => {
-  return <div>BuyerList</div>;
+  return (
+    <Box>
+      <Typography color="primary">He is a Buyer</Typography>
+    </Box>
+  );
 };
 
 export default BuyerList;

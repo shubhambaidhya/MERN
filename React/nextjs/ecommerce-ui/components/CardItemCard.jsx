3ff3ba42d1d3e499a35e0d65599efe9be3996f7f
shubhambaidhya <shubhambaidhya7@gmail.com>
2025-01-15 19:17:45 +0545

    Cart API hit + UI

--- React/nextjs/ecommerce-ui/components/CardItemCard.jsx
+++ React/nextjs/ecommerce-ui/components/CardItemCard.jsx
@@ -1,7 +1,96 @@
-import React from 'react';
+'use client';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import { Button, Checkbox, Chip, IconButton, Typography } from '@mui/material';
+import { Stack } from '@mui/system';
+import Image from 'next/image';
+import { useState } from 'react';
 
-const CardItemCard = () => {
-  return <div>CardItemCard</div>;
+const CartItemCard = (props) => {
+  const [count, setCount] = useState(props.orderedQuantity);
+
+  const increaseCount = () => setCount((prevCount) => prevCount + 1);
+  const decreaseCount = () =>
+    setCount((prevCount) => Math.max(prevCount - 1, 1));
+
+  return (
+    <div className="flex shadow-lg  w-[800px] justify-between gap-12 p-4 items-center">
+      <div className="w-1/2">
+        <Image
+          src={props?.productDetails?.image || '/iphone14.jpg'}
+          alt={props?.productDetails?.name}
+          width={350}
+          height={350}
+          style={{ objectFit: 'contain', maxHeight: '100%', maxWidth: '100%' }}
+        />
+      </div>
+
+      <div className="flex flex-col justify-center items-start gap-8 w-1/2 ml-8">
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          color="secondary"
+          sx={{ textTransform: 'capitalize' }}
+        >
+          {props?.productDetails?.name}
+        </Typography>
+        <Chip
+          label={props?.productDetails?.brand}
+          color="secondary"
+          sx={{ fontSize: '0.875rem', width: 100 }}
+        />
+        <Typography
+          variant="body1"
+          color="text.secondary"
+          textTransform="capitalize"
+        >
+          Category: <strong>{props?.productDetails?.category}</strong>
+        </Typography>
+        <Typography variant="body1" color="text.secondary">
+          Price: <strong>${props?.productDetails?.price}</strong>
+        </Typography>
+        <Stack
+          direction="row"
+          justifyContent="start"
+          alignItems="center"
+          gap={2}
+        >
+          <Typography variant="body1" color="text.secondary">
+            Free Shipping
+          </Typography>
+          <Checkbox
+            color="success"
+            checked={props?.productDetails?.freeShipping}
+          />
+        </Stack>
+        <div className="flex justify-center items-center gap-4">
+          <IconButton
+            color="success"
+            size="medium"
+            onClick={increaseCount}
+            aria-label="Increase quantity"
+          >
+            <AddIcon />
+          </IconButton>
+          <Typography variant="h6" sx={{ fontWeight: 'bold', color: '#333' }}>
+            {count}
+          </Typography>
+          <IconButton
+            color="error"
+            size="medium"
+            onClick={decreaseCount}
+            aria-label="Decrease quantity"
+            disabled={count === 1}
+          >
+            <RemoveIcon />
+          </IconButton>
+        </div>
+        <Button variant="contained" color="error">
+          Remove item
+        </Button>
+      </div>
+    </div>
+  );
 };
 
-export default CardItemCard;
+export default CartItemCard;
--- React/nextjs/ecommerce-ui/hooks/useCartList.js
+++ React/nextjs/ecommerce-ui/hooks/useCartList.js
@@ -1,16 +1,10 @@
-import $axios from '@/lib/axios/axios.instance';
+import { getCartList } from '@/lib/routes/cart.routes';
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
 
 const useCartList = () => {
   const { data, isPending, error, isError } = useQuery({
     queryKey: ['cart-list'],
-    queryFn: async () => {
-      return await $axios.post('/cart/list', {
-        page: 1,
-        limit: 10,
-      });
-    },
+    queryFn: getCartList(),
   });
 
   return { res: data, isPending, error, isError };
